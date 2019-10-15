6d56e308816b306bd3ad0e31c2bf1f5b678a244b
pythonredhat <pythonredhat1987@gmail.com>
2019-10-15 17:25:34 -0400

    Add get version functionality and pull version lord version

--- version_scraper_apache_kafka/__main__.py
+++ version_scraper_apache_kafka/__main__.py
@@ -5,17 +5,24 @@ from configparser import ConfigParser
 from importlib import resources
 
 def main():
-    #get config
     cfg = ConfigParser()
     cfg.read_string(resources.read_text("version_scraper_apache_kafka", "config.txt"))
-    url = cfg.get("test", "url")
+    url_kafka = cfg.get("test", "url_kafka")
+    url_version_lord = cfg.get("test", "url_version_lord")
 
-    kafkapage = requests.get(url)
-    soup = BeautifulSoup(kafkapage,content, 'html.parser')
+    kafka_page = requests.get(url_kafka)
+    soup = BeautifulSoup(kafka_page.content, 'html.parser')
 
-    kafka_release = soup.find_all('span')[1]
+    kafka_release = soup.find_all('span')[0]
 
-    print(kafka_release)
+    print(kafka_release['id'])
+
+    version_lord_page = requests.get(url=url_version_lord)
+    
+    json_data = json.loads(version_lord_page.text)
+
+    print(json_data)
+
 
 if __name__ == "__main__":
     main()
