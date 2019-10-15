0e88a08c90402ce4ef2c919b12d362986e7fe2cf
pythonredhat <pythonredhat1987@gmail.com>
2019-10-15 16:43:25 -0400

    Add skeleton

--- version_scraper_apache_kafka/__main__.py
+++ version_scraper_apache_kafka/__main__.py
@@ -1,21 +1,21 @@
 import requests
 import json
+from bs4 import BeautifulSoup
 from configparser import ConfigParser
 from importlib import resources
 
 def main():
     #get config
     cfg = ConfigParser()
-    cfg.read_string(resources.read_text("hello_world_app", "config.txt"))
+    cfg.read_string(resources.read_text("version_scraper_apache_kafka", "config.txt"))
     url = cfg.get("test", "url")
-    
-    #test code
-    response = requests.get(url=url)
-    print (response.status_code)
-    print (response.content)
-    json_data = json.loads(response.text)
-    github_latest_version = (json_data["name"])
-    print(f"Latest version on Github is {github_latest_version}")
+
+    kafkapage = requests.get(url)
+    soup = BeautifulSoup(kafkapage,content, 'html.parser')
+
+    kafka_release = soup.find_all('span')[1]
+
+    print(kafka_release)
 
 if __name__ == "__main__":
     main()
