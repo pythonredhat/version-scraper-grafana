26bd25672e4ddf5f0d61e959e3de4cc810804e1e
pythonredhat <pythonredhat1987@gmail.com>
2019-10-22 15:47:41 -0400

    Add .gitignore file

--- version_scraper_apache_kafka/__main__.py
+++ version_scraper_apache_kafka/__main__.py
@@ -4,6 +4,11 @@ from bs4 import BeautifulSoup
 from configparser import ConfigParser
 from importlib import resources
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s", filename="./logs/test.log")
+
+
 def main():
     cfg = ConfigParser()
     cfg.read_string(resources.read_text("version_scraper_apache_kafka", "config.txt"))
@@ -21,14 +26,17 @@ def main():
     
     json_data = json.loads(version_lord_page.text)
 
-    if json_data['current_version'] == kafka_release_version:
-        print("All good in the hood!")
-    else:
-        print("Version Lord is not up to date with RHEL site")
-        data = {'current_version': kafka_release_version, 'software': 'Apache Kafka'}
-        payload = requests.put(url=url_version_lord, data=json.dumps(data), headers={'Content-Type': 'application/json'})
-        print(payload.status_code)
-        print(payload.content)
+    try:
+        if json_data['current_version'] == kafka_release_version:
+            print("All good in the hood!") 
+        else:
+            print("Version Lord is not up to date with RHEL site")
+            data = {'current_version': kafka_release_version, 'software': 'Apache Kafka'}
+            payload = requests.put(url=url_version_lord, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+            logging.debug(payload.status_code)
+            logging.debug(payload.content)
+    except Exception as ex:
+        logging.exception('caught an error')
 
 if __name__ == "__main__":
     main()
