99e034ef9f09d338f11fbdadd2ca2aeb9d40810c
pythonredhat <pythonredhat1987@gmail.com>
2019-11-07 06:26:22 -0500

    Refactored package

--- version_scraper_grafana/config.py
+++ version_scraper_grafana/config.py
@@ -1,4 +1,3 @@
 software = "Grafana"
-url_kafka =  "https://kafka.apache.org/downloads"
-url_version_lord = "http://localhost:8000/api/v1/version_lord/9"
+url_version_lord = "http://localhost:8000/api/v1/version_lord/7"
 url_github_api = "https://api.github.com/repos/grafana/grafana/releases/latest"
