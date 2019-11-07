5c73195632d020760b62ebb020058c260e3112d2
pythonredhat <pythonredhat1987@gmail.com>
2019-11-07 06:10:05 -0500

    Change get_latest module

--- version_scraper_grafana/config.py
+++ version_scraper_grafana/config.py
@@ -1,3 +1,4 @@
-software = "Apache Kafka"
+software = "Grafana"
 url_kafka =  "https://kafka.apache.org/downloads"
 url_version_lord = "http://localhost:8000/api/v1/version_lord/9"
+url_github_api = "https://api.github.com/repos/grafana/grafana/releases/latest"
